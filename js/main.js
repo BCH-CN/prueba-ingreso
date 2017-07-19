bf96b0494efea53303edb90c17d9b0c11c62ef0b
Juan Ramirez <jua.ramirezp@gmail.com>
2017-07-18 20:53:33 -0400

    Pausa del Proyecto

--- js/main.js
+++ js/main.js
@@ -1,1 +1,44 @@
+var app = angular.module("banco", ["ngTable"]);
 
+app.controller("bancoController", function ($scope, ngTableParams) {
+    $scope.register=[
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:200000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:100000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:50000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:150000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:200000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:100000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:50000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:150000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:200000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:100000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:50000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:150000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:200000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:100000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:50000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:150000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:200000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:100000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:50000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:150000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:200000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:100000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:50000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:150000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:200000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:100000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:50000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:150000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:200000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:100000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:50000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:150000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:200000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:100000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:50000},
+        {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:150000}
+    ];
+
+    $scope.registerTable = new ngTableParams({}, { dataset: $scope.register });
+});
