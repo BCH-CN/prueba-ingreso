0493b77049aa1028780406d024b85ea465b80074
Juan Ramirez <jua.ramirezp@gmail.com>
2017-07-19 01:24:07 -0400

    Termino de maqueta propuesta

--- js/main.js
+++ js/main.js
@@ -40,5 +40,55 @@ app.controller("bancoController", function ($scope, ngTableParams) {
         {lorem:'xxxxxxxxxxxx',ipsum:'xxxxxxxxxxxx', dolor:'xxxxxxxxxxxx', sit:'xxxxxxxxxxxx', amet:150000}
     ];
 
-    $scope.registerTable = new ngTableParams({}, { dataset: $scope.register });
+    $scope.currentPage = 0;
+    $scope.pageSize = 10;
+    $scope.pages = [];
+
+    $scope.configPages = function() {
+        $scope.pages.length = 0;
+        var ini = $scope.currentPage - 4;
+        var fin = $scope.currentPage + 5;
+        if(ini < 1){
+            ini = 1;
+            if (Math.ceil($scope.register.length / $scope.pageSize) > 10){
+                fin = 10;
+            }else{
+                fin = Math.ceil($scope.register.length / $scope.pageSize);
+            } 
+        } else {
+            if (ini >= Math.ceil($scope.register.length / $scope.pageSize) - 10) {
+                ini = Math.ceil($scope.register.length / $scope.pageSize) - 10;
+                fin = Math.ceil($scope.register.length / $scope.pageSize);
+            }
+        }
+        if(ini < 1){
+          ini = 1;
+        } 
+        for(var i = ini; i <= fin; i++) {
+            $scope.pages.push({ no: i });
+        }
+        if($scope.currentPage >= $scope.pages.length){
+            $scope.currentPage = $scope.pages.length - 1;
+        }
+    };
+    
+    $scope.setPage = function(index) {
+        $scope.currentPage = index - 1;
+    };
+
+    $scope.configPages();
+});
+
+app.filter('startFromGrid', function() {
+    return function(input, start) {
+        start = +start;
+        return input.slice(start);
+    };
+});
+
+
+// Jquery
+
+$(document).ready(function(){
+    $('.datetimepicker').datepicker();
 });
